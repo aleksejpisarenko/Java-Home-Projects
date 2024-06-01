986228cacd928d89239ae32385ae7b25a4591ede
Aleksej Pisarenko <112798111+aleksejpisarenko@users.noreply.github.com>
2024-06-01 23:51:40 +0300

    Casual Update
    Added "Date Modified" row to a file. Basically, when you write or rewrite the file, the date string is added below your file content.
    Also, added option to exit to menu from write method while choosing options.

--- ReadWriteFile/FileReadWrite.java
+++ ReadWriteFile/FileReadWrite.java
@@ -1,9 +1,11 @@
 package ReadWriteFile;
 
 import java.io.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.time.*;
 
-public class FileReadWrite {
+public class FileReadWrite implements FileReadableWritable {
     public static void main(String[] args)  {
         System.out.println("ENTER 'R' TO READ A FILE, 'W' TO WRITE TO FILE OR 'E' TO EXIT THE PROGRAM");
         Scanner scanner = new Scanner(System.in);
@@ -20,10 +22,10 @@ public class FileReadWrite {
             line = line.toLowerCase();
         }
 
-        defineMethod(line);
+        new FileReadWrite().defineMethod(line);
     }
 
-    private static void defineMethod(String line)  {
+    public void defineMethod(String line)  {
         if(line.equals("r")) {
             read();
         } else {
@@ -31,19 +33,28 @@ public class FileReadWrite {
         }
     }
 
-    private static void write()  {
+    public void write()  {
         System.out.println("NOW CHOOSE WHAT YOU WANT TO DO");
         System.out.println("IF YOU WANT TO FULLY REWRITE FILE TYPE 'F'");
+        System.out.println();
         System.out.println("IF YOU WANT TO APPEND TO FILE TYPE 'A'");
+        System.out.println();
+        System.out.println("IF YOU WANT TO GO BACK TO MENU TYPE 'M'");
+        System.out.println();
 
         Scanner console = new Scanner(System.in);
         String option = console.nextLine();
         option = option.toLowerCase();
 
-        while(!option.equals("a") && !option.equals("f")) {
-            System.out.println("PLEASE ENTER 'F' TO REWRITE AND 'A' TO APPEND");
+        while(!option.equals("a") && !option.equals("f") && !option.equals("m")) {
+            System.out.println("PLEASE ENTER 'F' TO REWRITE AND 'A' TO APPEND OR 'M' TO EXIT TO MENU");
             option = console.nextLine();
             option = option.toLowerCase();
+        }
+
+        if(option.equals("m")) {
+            System.out.println();
+            main(new String[] {});
         }
 
         File file = getFile();
@@ -57,12 +68,11 @@ public class FileReadWrite {
                 String message = c.nextLine();
 
                 bufferedWriter.write(STR." \n\{message}");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy - HH:mm:ss");
+                bufferedWriter.write(STR."\n\tDate Modified \{new String(LocalDateTime.now().format(formatter))}\n");
             } catch (IOException e) {
                 System.out.println(STR."Something went wrong : \{e}");
             }
-
-            System.out.println();
-            main(new String[] {});
         } else {
             try(FileWriter fileWriter = new FileWriter(file);
                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter))
@@ -72,16 +82,18 @@ public class FileReadWrite {
                 String message = c.nextLine();
 
                 bufferedWriter.write(message);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy - HH:mm:ss");
+                bufferedWriter.write(STR."\n\tDate Modified  \{LocalDateTime.now().format(formatter)}");
             } catch (IOException e) {
-                System.out.println(STR."Something went wrong : \{e}");
+                System.out.println(STR."Somethieng went wrong : \{e}");
             }
-
-            System.out.println();
-            main(new String[] {});
         }
+
+        System.out.println();
+        main(new String[] {});
     }
 
-    private static void read() {
+    public void read() {
         System.out.println("PLEASE ENTER PATH");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
